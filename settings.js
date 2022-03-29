8caeb9b5cf41c13eb829798157825319783acdee
RohisGanz <99573558+Rohisbo@users.noreply.github.com>
2022-03-29 07:06:06 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,9 +12,9 @@ global.APIKeys = {
 }
 
 // setting 
-global.autoread = true // auto read pesan / message
+global.autoread = false // auto read pesan / message
 global.autorecording = true //status auto merekam ( auto record )
-global.autoketik = true //status auto mengetik (auto typing)
+global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 
 // Other
